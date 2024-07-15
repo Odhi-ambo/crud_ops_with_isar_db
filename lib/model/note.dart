17260588e35a70a562724e1f53ad08454a1b0813
Odhi-ambo <michaelderrick605@gmail.com>
2024-07-15 09:43:25 +0300

    Initial commit

--- lib/model/note.dart
+++ lib/model/note.dart
@@ -1,5 +1,8 @@
 import 'package:isar/isar.dart';
 
+part 'note.g.dart';//needed to generate file//then run flutter pub run build_runner build 
+
 class Note {
   Id id = Isar.autoIncrement;
+  late String text;
 }
