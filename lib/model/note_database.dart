00cb5e606ab7254d9161f2f8c74ce46112ce23c1
Odhi-ambo <michaelderrick605@gmail.com>
2024-07-15 16:14:21 +0300

    Initial commit

--- lib/model/note_database.dart
+++ lib/model/note_database.dart
@@ -1,8 +1,17 @@
+import 'package:crud_isar_db/model/note.dart';
 import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
 
 class NoteDatabase {
   static late Isar isar;
   //initialize db
+  static Future<void> init() async {
+    final dir = await getApplicationDocumentsDirectory();
+    isar = await Isar.open(
+      [NoteSchema],
+      directory: dir.path,
+    );
+  }
 
   //create
 
