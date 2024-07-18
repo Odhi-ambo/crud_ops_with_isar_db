a9138c3b9bbd69c11ebe40ad0be77a144b105025
Odhi-ambo <michaelderrick605@gmail.com>
2024-07-18 10:42:53 +0300

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:crud_isar_db/model/note_database.dart';
 import 'package:crud_isar_db/pages/note_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   //initialize note isar db
   WidgetsFlutterBinding.ensureInitialized();
   await NoteDatabase.initialize();
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(create: (context) => NoteDatabase(), child: MyApp()),
+  );
 }
 
 class MyApp extends StatelessWidget {
