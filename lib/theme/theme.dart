af6e186b6b300db272d8ae1c2c2e755f3c6ddbcb
Odhi-ambo <michaelderrick605@gmail.com>
2024-07-23 14:26:25 +0300

    Initial commit

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -5,6 +5,9 @@ import 'package:flutter/material.dart';
 ThemeData lightMode = ThemeData(
     brightness: Brightness.light,
     colorScheme: ColorScheme.light(
-        surface: Colors.grey.shade300, primary: Colors.grey.shade200));
+        surface: Colors.grey.shade300,
+        primary: Colors.grey.shade200,
+        secondary: Colors.grey.shade400,
+        inversePrimary: Colors.grey.shade800));
 
 //dark mode
