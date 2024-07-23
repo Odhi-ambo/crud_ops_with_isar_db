c5c29e9553a8518c7158a86550092ae9ebf45f7b
Odhi-ambo <michaelderrick605@gmail.com>
2024-07-23 14:24:06 +0300

    Initial commit

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 
 //light theme
 
-ThemeData lightMode = ThemeData();
+ThemeData lightMode = ThemeData(
+    brightness: Brightness.dark,
+    colorScheme: ColorScheme.dark(
+        background: Colors.grey.shade300, primary: Colors.grey.shade200));
 
 //dark mode
