2193212e02005d2750d7567a62beebc4f965f746
Odhi-ambo <michaelderrick605@gmail.com>
2024-07-23 14:29:06 +0300

    Initial commit

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -11,4 +11,10 @@ ThemeData lightMode = ThemeData(
         inversePrimary: Colors.grey.shade800));
 
 //dark mode
-ThemeData darkMode = ThemeData();
+ThemeData darkMode = ThemeData(
+    brightness: Brightness.dark,
+    colorScheme: ColorScheme.dark(
+        surface: Colors.grey.shade900,
+        primary: Colors.grey.shade800,
+        secondary: Colors.grey.shade700,
+        inversePrimary: Colors.grey.shade300));
