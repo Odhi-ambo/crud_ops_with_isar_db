f40e1fdd52d6ec4d046afde0c31951b5ddc71440
Odhi-ambo <michaelderrick605@gmail.com>
2024-07-24 09:35:39 +0300

    Initial commit

--- lib/theme/theme_provider.dart
+++ lib/theme/theme_provider.dart
@@ -1,5 +1,11 @@
+import 'package:crud_isar_db/theme/theme.dart';
 import 'package:flutter/material.dart';
 
 class ThemeProvider with ChangeNotifier {
-  //
+  //initial theme
+  ThemeData _themeData = lightMode;
+
+  ThemeData get themeData => _themeData;
+
+  bool get isDarkMode => _themeData == darkMode;
 }
