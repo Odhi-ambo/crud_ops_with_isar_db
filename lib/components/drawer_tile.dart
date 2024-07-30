715752f4a6779d87e3dda83181b5f2bc45197c94
Odhi-ambo <michaelderrick605@gmail.com>
2024-07-30 08:29:37 +0300

    Initial commit

---
+++ lib/components/drawer_tile.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DrawerTile extends StatelessWidget {
+  const DrawerTile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile();
+  }
+}
