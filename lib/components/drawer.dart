76e654407e4e397adaefee0a15a08182af6bb9cb
Odhi-ambo <michaelderrick605@gmail.com>
2024-07-31 08:19:52 +0300

    Initial commit

--- lib/components/drawer.dart
+++ lib/components/drawer.dart
@@ -1,3 +1,4 @@
+import 'package:crud_isar_db/components/drawer_tile.dart';
 import 'package:flutter/material.dart';
 
 class MyDrawer extends StatelessWidget {
@@ -7,13 +8,18 @@ class MyDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Drawer(
       backgroundColor: Theme.of(context).colorScheme.surface,
-      child: const Column(
+      child: Column(
         children: [
           //header
           DrawerHeader(
             child: Icon(Icons.note),
           ),
           //notes title
+          DrawerTile(
+            leading: Icon(Icons.note),
+            title: 'Notes',
+            onTap: () {},
+          )
 
           //notes settings
         ],
