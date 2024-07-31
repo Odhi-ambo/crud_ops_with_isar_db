9735253ffbf0a6dd7fa062a1ec4cb3e38badc94d
Odhi-ambo <michaelderrick605@gmail.com>
2024-07-31 08:15:46 +0300

    Initial commit

--- lib/components/drawer_tile.dart
+++ lib/components/drawer_tile.dart
@@ -3,12 +3,20 @@ import 'package:flutter/material.dart';
 class DrawerTile extends StatelessWidget {
   final String title;
   final Widget leading;
-  const DrawerTile({super.key, required this.leading, required this.title});
+  final void Function()? onTap;
+
+  const DrawerTile(
+      {super.key,
+      required this.leading,
+      required this.title,
+      required this.onTap});
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(title),
+      leading: leading,
+      onTap: onTap,
     );
   }
 }
