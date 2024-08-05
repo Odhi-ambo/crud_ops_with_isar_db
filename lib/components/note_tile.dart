477a0e8123dd089acb623963df281ddabddfc651
Odhi-ambo <michaelderrick605@gmail.com>
2024-08-05 14:10:40 +0300

    Initial commit

---
+++ lib/components/note_tile.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NoteTile extends StatelessWidget {
+  const NoteTile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
