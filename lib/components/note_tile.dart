f56cb45456757551b5c0195213549b78684b1e5f
Odhi-ambo <michaelderrick605@gmail.com>
2024-08-12 10:06:28 +0300

    Initial commit

--- lib/components/note_tile.dart
+++ lib/components/note_tile.dart
@@ -24,6 +24,8 @@ class NoteTile extends StatelessWidget {
           trailing: IconButton(
             icon: const Icon(Icons.more_vert),
             onPressed: () => showPopover(
+              width: 100,
+              height: 100,
               context: context,
               bodyBuilder: (context) => NoteSettings(),
             ),
