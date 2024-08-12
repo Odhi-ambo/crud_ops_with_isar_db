22c330930d36068dda8f02929ffcb6669a0d66bf
Odhi-ambo <michaelderrick605@gmail.com>
2024-08-12 11:24:59 +0300

    Initial commit

--- lib/components/note_settings.dart
+++ lib/components/note_settings.dart
@@ -5,8 +5,6 @@ class NoteSettings extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
-    );
+    return Column();
   }
 }
